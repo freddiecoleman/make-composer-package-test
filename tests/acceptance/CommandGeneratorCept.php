3a278fd82c414c5281da2444d90ef80f445b430a
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-11 20:57:08 +0100

    building tests

--- tests/acceptance/CommandGeneratorCept.php
+++ tests/acceptance/CommandGeneratorCept.php
@@ -1,3 +1,9 @@
 <?php 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('generate both a command and a handler class.');
+
+$I->runShellCommand('php artisan commander:generate FooCommand --properties="bar, baz" --base="tests/tmp"');
+
+$I->seeInShellOutput('All done!');
+$I->seeFileFound('tests/tmp/FooCommand.php');
+$I->seeFileFound('tests/tmp/FooCommandHandler.php');
