c11ad3ebaba28a2960296500f39a26a0a06f4e99
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-08-11 21:21:02 +0100

    built stub and test to check against stub

--- tests/acceptance/CommandGeneratorCept.php
+++ tests/acceptance/CommandGeneratorCept.php
@@ -5,5 +5,6 @@ $I->wantTo('generate both a command and a handler class.');
 $I->runShellCommand('php artisan commander:generate FooCommand --properties="bar, baz" --base="tests/tmp"');
 
 $I->seeInShellOutput('All done!');
-$I->seeFileFound('tests/tmp/FooCommand.php');
-$I->seeFileFound('tests/tmp/FooCommandHandler.php');
+
+$I->openFile('tests/tmp/FooCommand.php');
+$I->seeFileContentsEqual(file_get_contents('tests/acceptance/stubs/FooCommand.stub'));
